c47cb87837007c9efe68712f8b48b52be11bceab
Jonathan Azoff <jon@azoffdesign.com>
2012-08-13 17:24:38 -0700

    fixed bug in jpg.to plugins

--- plugins/jpg.js
+++ plugins/jpg.js
@@ -3,7 +3,7 @@
 	"use strict";
 
 	exports.message = function(from, message, callback) {
-		var slug = message.replace(/\W+/, '-');
+		var slug = message.replace(/\W+/g, '-');
 		callback(null, 'http://' + slug + '.jpg.to');
 	};
 	
