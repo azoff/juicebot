fc85b36941b9e3e69384a545267e1111cf5e6bf6
Jonathan Azoff <jon@azoffdesign.com>
2012-08-15 14:21:58 -0700

    added insult plugin

---
+++ plugins/insult.js
@@ -0,0 +1,37 @@
+(function(exports){
+	
+	"use strict";
+	
+	var http   = require('http');
+	var jsdom  = require('jsdom');
+	var jquery = require('jquery');
+
+	function getjQuery(window) {
+		return function(selector, context) {
+			return jquery(selector, context || window.document);
+		}
+	}
+
+	function respond(message, html, callback) {
+		jsdom.env(html, function(error, window) {
+			if (error) { callback(error); }			
+			else {
+				var $ = getjQuery(window);
+				var insult = $('td:first').text().trim();
+				callback(null, message + ', ' + insult);
+			}
+		});
+	}
+
+	exports.message = function(from, message, callback) {	
+		var html = '';
+		var request = http.get('http://www.insultgenerator.org', function(response){
+			response.setEncoding('utf8');
+			response.on('data', function(chunk){ html += chunk; });
+			response.on('end', function(){ 
+				respond(message, html, callback); 
+			});
+		}).on('error', callback);
+	};
+
+})(this);
