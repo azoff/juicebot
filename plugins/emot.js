065adedb28fd7fe9dca55294ed809817fb616f62
Jonathan Azoff <jon@azoffdesign.com>
2012-08-20 11:45:42 -0700

    fixed and renamed emoticon plugin

--- plugins/emot.js
+++ plugins/emot.js
@@ -7,14 +7,27 @@
 	var jquery = require('jquery');
 	var async  = require('async');
 
+	var office_emoticons = [
+		'(alabamuh)',
+		'(angrydavid)',
+		'(azoff)',
+		'(chiefkunal)',
+		'(dunkman)',
+		'(fearthebeard)',
+		'(kunal)',
+		'(rentjuice)',
+		'(summer)',
+		'(smilydan)'
+	];
+
 	function getjQuery(window) {
 		return function(selector, context) {
 			return jquery(selector, context || window.document);
 		}
 	}
 
-	function getText(filter, i, shortcut) {
-		var command = jquery(shortcut).text().trim();
+	function getText(filter, i, e) {
+		var command = i.substr ? i : jquery(e).text().trim();
 		var name = command.replace(/\W+/g, '');
 		return (filter.test(command) && name.length > 2) ?
 			(name + ' ' + command) :
@@ -26,28 +39,15 @@
 			if (error) { callback(error); }			
 			else {
 				var $ = getjQuery(window);
-				var filter = message !== 'recent' ? new RegExp(message || '.*', 'i') : /.*/;
-				var mapper = async.apply(getText, filter);
-				var shortcuts = jquery.makeArray($('.emoticons:first .shortcut').map(mapper));
-				var custom_emoticons = {
-					'(alabamuh)': 'brian seitel',
-					'(angrydavid)': 'david vivero',
-					'(azoff)': 'jon azoff',
-					'(chiefkunal)': 'kunal shah',
-					'(dunkman)': 'nick dunkman',
-					'(fearthebeard)': 'matt gidney',
-					'(kunal)': 'kunal shah'
-				};
-				for (command in custom_emoticons) {
-					// match command or person's actual name
-					if (command.match(filter) || custom_emoticons[command].match(filter)) {
-						shortcuts.push(command.replace(/\W+/g, '') + ' ' + command);
-					}
-				}
+				var filter    = message !== 'recent' ? new RegExp(message || '.*', 'i') : /.*/;
+				var mapper    = async.apply(getText, filter);
+				var matches   = $('.emoticons:first .shortcut');
+				var shortcuts = jquery.makeArray(matches.map(mapper));
 				if (message === 'recent') {
 					shortcuts = shortcuts.slice(0, 5);
-				} else {	
-					shortcuts = shortcuts.sort();
+				} else {
+					matches = office_emoticons.map(mapper);
+					shortcuts = shortcuts.concat(matches).sort();
 				}
 				if (shortcuts.length) {
 					async.forEach(shortcuts, async.apply(callback, null));
